176890e21ef89abf46099d7d384da81ee67ef546
Tim Welch <tim.j.welch@gmail.com>
2014-11-23 14:26:10 -0800

    no unfeatured tools in list

--- caribnode/tools/templatetags/tools_tags.py
+++ caribnode/tools/templatetags/tools_tags.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 @register.assignment_tag(takes_context=True)
 def featured_tools(context, count=7):
-    tools = Tool.objects.order_by("featured").order_by("id")[:count]
+    tools = Tool.objects.filter(featured=True).order_by("featured").order_by("id")[:count]
     return tools
 
 @register.assignment_tag(takes_context=True)
